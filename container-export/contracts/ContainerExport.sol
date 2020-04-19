e62bde3eb064e3fa84d491cb97419bb3b18f1300
Lasse97 <lasseboladewig@gmail.com>
2020-04-19 12:52:09 +0200

    Update ContainerExport

--- container-export/contracts/ContainerExport.sol
+++ container-export/contracts/ContainerExport.sol
@@ -3,4 +3,14 @@ pragma solidity >= 0.5.0 < 0.7.0;
 contract ContainerExport {
   constructor() public {
   }
+  struct Company {
+    string name;
+    uint corporateId;
+  }
+
+  Company[] public companies;
+
+  function newCompany(string _name, uint _corporateId){
+    companies.push(_name, _corporateId);
+  }
 }
