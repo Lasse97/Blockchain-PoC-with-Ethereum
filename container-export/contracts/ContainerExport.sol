5bdd786febd4720f28bdbb855cf2561aa6deae2b
Lasse97 <lasseboladewig@gmail.com>
2020-05-03 19:55:50 +0200

    Add Actors

--- container-export/contracts/ContainerExport.sol
+++ container-export/contracts/ContainerExport.sol
@@ -1,34 +1,42 @@
 pragma solidity >= 0.5.0 < 0.7.0;
 
-contract ContainerExport {
+import "./Owner.sol";
 
-  event NewCompany(string name, uint corporateId);
+contract ContainerExport is Owner {
+
+  event NewCompany(string name, uint corporateId, Actors _actors);
+
+  enum Actors { trucker, forwarder, terminal }
 
   struct Company {
     string name;
     uint corporateId;
+    Actors actors;
   }
 
   mapping (address => Company) public companies;
-  mapping (address => uint) ownerCompanyCount;
 
   address[] public companyAddresses;
 
-  function createCompany(string memory _name, uint _corporateId) public {
-    require(ownerCompanyCount[msg.sender] == 0);
+  function createCompany(string memory _name, uint _corporateId, Actors _actors) public {
+# ist die Überprüfung noch notwendig, wenn ich den Owner habe? eher ja, weil so verhinntert
+# wird, dass mehr als eine Company angemeldet werden kann
+    for (uint i=0; i < companyAddresses.length; i++) {
+        require(msg.sender != companyAddresses[i]);
+    }
     companies[msg.sender].name = _name;
     companies[msg.sender].corporateId = _corporateId;
-    ownerCompanyCount[msg.sender]++;
-    emit NewCompany(_name, _corporateId);
+    companies[msg.sender].actors = _actors;
+    emit NewCompany(_name, _corporateId, _actors);
     companyAddresses.push(msg.sender);
   }
 
-  function getOwnerCompanyCount() public view returns (uint) {
-    return (ownerCompanyCount[msg.sender]);
+  function getAllCompanies() external view returns (address[] memory) {
+    return companyAddresses;
   }
 
-  function getAllCompanies() external view returns (address[] memory) {
-      return companyAddresses;
+  function getCompanyActor() external view returns (Actors) {
+    return companies[msg.sender].actors;
   }
 
 }
