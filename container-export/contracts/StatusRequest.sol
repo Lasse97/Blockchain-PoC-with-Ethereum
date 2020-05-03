b3f20b665b4b5ce0056ed191f99dac8796f38266
Lasse97 <lasseboladewig@gmail.com>
2020-05-03 19:56:06 +0200

    Add StatusRequest

---
+++ container-export/contracts/StatusRequest.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.5.0 < 0.7.0;
+
+import "./ContainerExport.sol";
+
+contract StatusRequest is ContainerExport {
+
+  struct StatusInformation {
+  bool containerKnown;
+  bool releaseNumberKnown;
+  bool clearedCustoms;
+  bool containerDischarged;
+  }
+
+}
